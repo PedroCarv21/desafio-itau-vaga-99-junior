b96677d13d0a67097b578aa02820364815c47602
Pedro Carvalho <pedro8.carvalho@gmail.com>
2025-10-26 19:10:52 -0300

    criacao da aplicacao com Swagger

---
+++ src/main/java/com/dev/desafio_itau_vaga_99_junior/Application.java
@@ -0,0 +1,13 @@
+package com.dev.desafio_itau_vaga_99_junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/desafio_itau_vaga_99_junior/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.desafio_itau_vaga_99_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
